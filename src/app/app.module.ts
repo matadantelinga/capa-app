aeff85db9d49afdf4ab27bd06013443bf450b931
matadantelinga <indrayanasoetedja@gmail.com>
2022-10-05 00:54:06 +0700

    declare video modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { VideoModalComponent } from './components/video-modal/video-modal.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, VideoModalComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
